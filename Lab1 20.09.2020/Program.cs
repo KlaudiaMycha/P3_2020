c66ab40e14e11b20101ab3da27b7444ca6c9003a
KlaudiaCapikATH <kc055880@student.ath.edu.pl>
2020-09-22 20:53:04 +0200

    intra

--- Lab1 20.09.2020/Program.cs
+++ Lab1 20.09.2020/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Lab1
+namespace Lab1_20._09._2020
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" It's me! :) ");
+            Console.WriteLine("Hello everybody!:) ");
         }
     }
 }
